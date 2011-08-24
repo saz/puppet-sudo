2e757a8571f27f1026fe237ba4f486416beb23de
Saz <me@saz.sh>
2011-08-24 11:11:24 +0200

    fix missing path for sudoers.d, add priority to files in sudoers.d

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,11 +1,11 @@
-define sudo::conf($content = undef, $source = undef) {
+define sudo::conf($priority = 10, $content = undef, $source = undef) {
     include sudo
 
-    file { $name:
+    file { "${priority}_${name}":
         ensure  => present,
         owner   => root,
         group   => root,
-        mode    => 640,
+        mode    => 0440,
         source  => $source,
         content => $content,
     }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,7 @@ class sudo::params {
         /(Ubuntu|Debian)/: {
             $package_name = 'sudo'
             $sudoers = '/etc/sudoers'
+            $conf_dir = '/etc/sudoers.d/'
         }
     }
 }
