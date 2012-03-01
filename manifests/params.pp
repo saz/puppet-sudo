53b8d9b3ba59bb7d3008c54d26662c50fa172299
Matt Mencel <mr-mencel@wiu.edu>
2012-03-01 15:08:51 -0600

    adding redhat and centos

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,8 +4,14 @@ class sudo::params {
       $package = 'sudo'
       $config_file = '/etc/sudoers'
       $config_dir = '/etc/sudoers.d/'
-      $source = 'puppet:///modules/sudo/sudoers'
+      $source = 'puppet:///modules/sudo/sudoers.deb'
     }
+	redhat, centos: {
+	  $package = 'sudo'
+      $config_file = '/etc/sudoers'
+      $config_dir = '/etc/sudoers.d/'
+      $source = 'puppet:///modules/sudo/sudoers.rhel'
+	}
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
