498f032647a4224f006d9feb96d80e8f6d83bb61
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-08 22:14:54 +0300

    adding gentoo

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class sudo::params {
   case $::operatingsystem {
-    ubuntu, debian: {
+    ubuntu, debian, gentoo: {
       $package = 'sudo'
       $config_file = '/etc/sudoers'
       $config_dir = '/etc/sudoers.d/'
