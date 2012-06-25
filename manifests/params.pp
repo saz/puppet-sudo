d9e6adc87b4f829011dcbe5e54ff855a61b0ab03
Michael Stahnke <stahnma@puppetlabs.com>
2012-06-25 15:57:18 -0700

    Add support for Fedora
    Very simple commit just adds fedora into the OSes available for support.
    I also cleaned up a hard-tab and replaced it with a space.
    
    Signed-off-by: Michael Stahnke <stahnma@puppetlabs.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,12 +6,12 @@ class sudo::params {
       $config_dir = '/etc/sudoers.d/'
       $source = 'puppet:///modules/sudo/sudoers.deb'
     }
-    redhat, centos: {
+    redhat, centos, fedora: {
       $package = 'sudo'
       $config_file = '/etc/sudoers'
       $config_dir = '/etc/sudoers.d/'
       $source = 'puppet:///modules/sudo/sudoers.rhel'
-	}
+    }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
