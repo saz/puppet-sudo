b34a3de357458596b9b5f6b95d1014bba1ce28dd
Nic Waller <code@nicwaller.com>
2013-07-29 21:46:43 -0700

    Removed debugging notify and use class_name for portability

--- manifests/allow.pp
+++ manifests/allow.pp
@@ -59,16 +59,15 @@ class sudo::allow(
 ) {
   # TODO validate that all input is arrays
 
-  notify { "testing key ${module_name}": }
   if $replace_users != undef {
     $users = $replace_users
   } else {
-    $users = hiera_array("${module_name}::allow::add_users", $add_users)
+    $users = hiera_array("${module_name}::${class_name}::add_users", $add_users)
   }
   if $replace_groups != undef {
     $groups = $replace_groups
   } else {
-    $groups = hiera_array("${module_name}::allow::add_groups", $add_groups)
+    $groups = hiera_array("${module_name}::${class_name}::add_groups", $add_groups)
   }
 
   sudo::conf { 'sudo_users_groups':
