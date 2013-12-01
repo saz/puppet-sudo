51e9c0aa84cc927ed299f7dd0ff0de1f67e2fb2c
Steffen Zieger <me@saz.sh>
2013-12-01 15:51:00 +0100

    add spec tests

---
+++ spec/classes/sudo_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+describe 'sudo' do
+
+  let :default_params do
+    {
+      :ensure              => 'present',
+      :autoupgrade         => false,
+      :purge               => false,
+      :config_file_replace => false,
+    }
+  end
+
+  [ {},
+    {
+      :ensure              => 'present',
+      :autoupgrade         => false,
+      :purge               => true,
+      :config_file_replace => false,
+    },
+    {
+      :ensure              => 'present',
+      :autoupgrade         => true,
+      :purge               => false,
+      :config_file_replace => true,
+    }
+  ].each do |param_set|
+    describe "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+
+      let :param_hash do
+        default_params.merge(param_set)
+      end
+
+      let :params do
+        param_set
+      end
+
+      ['Debian', 'Redhat'].each do |osfamily|
+
+        let :facts do
+          {
+            :osfamily        => osfamily,
+          }
+        end
+
+        describe "on supported osfamily: #{osfamily}" do
+
+          it { should contain_class('sudo::params') }
+
+          it { 
+            if not param_hash[:autoupgrade]
+              should contain_package('sudo').with_ensure('present')
+            end
+          }
+
+          it {
+            if param_hash[:autoupgrade]
+              should contain_package('sudo').with_ensure('latest')
+            end
+          }
+
+          it {
+            if param_hash[:ensure] == 'present'
+              should contain_file('/etc/sudoers').with(
+                'ensure'  => 'present',
+                'owner'   => 'root',
+                'group'   => 'root',
+                'mode'    => '0440',
+                'replace' => param_hash[:config_file_replace],
+              )
+            end
+          }
+
+          it {
+            if param_hash[:ensure] == 'present'
+              should contain_file('/etc/sudoers.d/').with(
+                'ensure'  => 'directory',
+                'owner'   => 'root',
+                'group'   => 'root',
+                'mode'    => '0550',
+                'recurse' => param_hash[:purge],
+                'purge'   => param_hash[:purge],
+              )
+            end
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/defines/sudo_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+describe 'sudo::conf', :type => :define do
+  let(:title)    { 'admins' }
+  let(:filename) { '10_admins' }
+  let(:file_path) { '/etc/sudoers.d/10_admins' }
+
+  let :facts do
+    {
+      :osfamily => 'Debian'
+    }
+  end
+
+  let :params do
+      {
+      :priority        => 10,
+      :content         => "%admins ALL=(ALL) NOPASSWD: ALL",
+      :sudo_config_dir => '/etc/sudoers.d/',
+      }
+  end
+
+  describe "when creating a sudo entry" do
+
+    it { should contain_sudo__conf('admins').with({
+        :priority => params[:priority],
+        :content  => params[:content],
+      })
+    }
+
+    it { should contain_file(filename).with({
+        'ensure'  => 'present',
+        'content' => "%admins ALL=(ALL) NOPASSWD: ALL\n",
+        'owner'   => 'root',
+        'group'   => 'root',
+        'path'    => file_path,
+        'mode'    => '0440',
+      })
+    }
+
+  end
+
+  describe "when removing an sudo entry" do
+    let :params do
+      {
+      :ensure          => 'absent',
+      :priority        => 10,
+      :content         => "%admins ALL=(ALL) NOPASSWD: ALL",
+      :sudo_config_dir => '/etc/sudoers.d/',
+      }
+    end
+
+    it { should contain_file(filename).with({
+        'ensure'  => 'absent',
+        'content' => "%admins ALL=(ALL) NOPASSWD: ALL\n",
+        'owner'   => 'root',
+        'group'   => 'root',
+        'path'    => file_path,
+        'mode'    => '0440',
+      })
+    }
+  end
+end
