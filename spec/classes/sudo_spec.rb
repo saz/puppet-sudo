b314b499e0100383bf2885fba1f5139de933d971
Toni Schmidbauer <toni@stderr.at>
2014-02-16 22:31:12 +0100

    aix rspec tests

--- spec/classes/sudo_spec.rb
+++ spec/classes/sudo_spec.rb
@@ -66,6 +66,38 @@ describe 'sudo' do
 
         end
       end
+
+      describe "on supported osfamily: AIX" do
+
+        it { should contain_class('sudo::params') }
+
+        it {
+          if param_hash[:enable] == 'true'
+            should contain_file('/etc/sudoers').with(
+              'ensure'  => 'present',
+              'owner'   => 'root',
+              'group'   => 'system',
+              'mode'    => '0440',
+              'replace' => param_hash[:config_file_replace]
+            )
+            should contain_file('/etc/sudoers.d/').with(
+              'ensure'  => 'directory',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0550',
+              'recurse' => param_hash[:purge],
+              'purge'   => param_hash[:purge]
+            )
+            should contain_class('sudo::package').with(
+              'package' => 'sudo',
+              'package_ensure' => 'present',
+              'package_source' => 'http://www.oss4aix.org/compatible/aix53/sudo-1.8.7-1.aix5.1.ppc.rpm'
+            )
+
+          end
+        }
+
+      end
     end
   end
 end
