66389a0a8bc314da459ad22faa6f752ed707e126
Toni Schmidbauer <toni@stderr.at>
2014-02-22 21:42:41 +0100

    removed trailing comma in with()
    otherwise running rspec fails with ruby 1.8.7.

--- spec/classes/package_aix_spec.rb
+++ spec/classes/package_aix_spec.rb
@@ -23,7 +23,7 @@ describe 'sudo::package::aix' do
       should contain_package('sudo').with(
         'ensure'   => 'present',
         'source'   => 'http://www.oss4aix.org/compatible/aix53/sudo-1.8.7-1.aix5.1.ppc.rpm',
-        'provider' => 'rpm',
+        'provider' => 'rpm'
       )
     }
   end
