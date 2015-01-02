fedd0868f47da9823b4d9d5c54110b043fa3146a
Fabio Rauber <fabiorauber@gmail.com>
2015-01-02 09:25:27 -0200

    Added configs_hash variable to allow passing a configuration hash to sudo::configs

--- manifests/configs.pp
+++ manifests/configs.pp
@@ -6,15 +6,18 @@
 #
 # See the primary sudo module documentation for usage and examples.
 #
-class sudo::configs {
+class sudo::configs (
+    $configs_hash = {},
+  ){
 
+  validate_hash ( $configs_hash )
   # NOTE: hiera_hash does not work as expected in a parameterized class
   #   definition; so we call it here.
   #
   # http://docs.puppetlabs.com/hiera/1/puppet.html#limitations
   # https://tickets.puppetlabs.com/browse/HI-118
   #
-  $configs = hiera_hash('sudo::configs', undef)
+  $configs = hiera_hash('sudo::configs', $configs_hash)
 
   if $configs {
     create_resources('::sudo::conf', $configs)
