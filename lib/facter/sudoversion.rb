f90222f3ff25c4fd7e9ae4fb49a175d08bd69bee
Chris Roddy <cmr@mdc2.org>
2015-01-14 14:58:56 -0500

    Remove --nosignature and --nodigest
    By my reading of the rpm manual page, these options are not meaningful
    in query mode. For AIX users with very old versions of RPM, adding these
    options to the command line causes error output to be mailed with every
    run of the refresh-mcollective-metadata cron job.

--- lib/facter/sudoversion.rb
+++ lib/facter/sudoversion.rb
@@ -2,7 +2,7 @@ require 'puppet'
 Facter.add("sudoversion") do
   setcode do
     if Facter::Util::Resolution.which('rpm')
-      sudoversion = Facter::Util::Resolution.exec('rpm -q sudo --nosignature --nodigest --qf \'%{VERSION}\'')
+      sudoversion = Facter::Util::Resolution.exec('rpm -q sudo --qf \'%{VERSION}\'')
     end
   end
 end
