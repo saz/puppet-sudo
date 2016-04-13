1b6e9af48a078ff6976ddce349ff46869c5045a4
Joseph Yaworski <joseph.yaworski@fireeye.com>
2016-04-12 21:56:16 -0400

    sudoversion is less os-specific, add tests

--- lib/facter/sudoversion.rb
+++ lib/facter/sudoversion.rb
@@ -1,10 +1,12 @@
-require 'puppet'
-Facter.add("sudoversion") do
+Facter.add(:sudoversion) do
   setcode do
-    if Facter::Util::Resolution.which('rpm')
-      sudoversion = Facter::Util::Resolution.exec('rpm -q sudo --qf \'%{VERSION}\'')
-    elsif Facter::Util::Resolution.which('sudo') and Facter::Util::Resolution.which('awk')
-      sudoversion = Facter::Util::Resolution.exec('sudo -V | awk \'/Sudo version/ {print $3}\'')
+    if Facter::Util::Resolution.which('sudo')
+      sudoversion = Facter::Util::Resolution.exec('sudo -V 2>&1')
+      %r{^Sudo version ([\w\.]+)}.match(sudoversion)[1]
+    elsif Facter::Util::Resolution.which('rpm')
+ -    sudoversion = Facter::Util::Resolution.exec('rpm -q sudo --qf \'%{VERSION}\'')
+    elsif Facter::Util::Resolution.which('dpkg-query')
+ -    sudoversion = Facter::Util::Resolution.exec('dpkg-query -W -f=\'${Version}\n\' sudo')
     end
   end
 end
