59cf5578628fcadd49949229dd740245e5d5f47c
Kaleb Eckles <kaleb_eckles@anfcorp.com>
2019-05-14 13:53:01 -0400

    add package_aix_spec.rb to allow for package provider override

--- spec/classes/package_aix_spec.rb
+++ spec/classes/package_aix_spec.rb
@@ -3,9 +3,10 @@ describe 'sudo::package::aix' do
   describe 'on supported osfamily: AIX' do
     let :params do
       {
-        :package        => 'sudo',
-        :package_ensure => 'present',
-        :package_source => 'http://www.sudo.ws/sudo/dist/packages/AIX/5.3/sudo-1.8.9-6.aix53.lam.rpm'
+        :package                   => 'sudo',
+        :package_ensure            => 'present',
+        :package_source            => 'http://www.sudo.ws/sudo/dist/packages/AIX/5.3/sudo-1.8.9-6.aix53.lam.rpm'
+        :package_provider_override => 'rpm'
       }
     end
 
@@ -23,4 +24,28 @@ describe 'sudo::package::aix' do
       )
     end
   end
+
+  let :params do
+    {
+      :package                   => 'sudo',
+      :package_ensure            => 'present',
+      :package_source            => ''
+      :package_provider_override => 'yum'
+    }
+  end
+
+  let :facts do
+    {
+      :osfamily => 'AIX'
+    }
+  end
+
+  it do
+    is_expected.to contain_package('sudo').with(
+      'ensure'   => 'present',
+      'source'   => '',
+      'provider' => 'yum'
+    )
+  end
+end
 end
